7e0c95e7472ef7278efe83b46a1119039d048483
TeoEchavarria <echavarria.0042@gmail.com>
2024-10-27 10:16:09 -0500

    Add GitHub Actions workflow for running the Telegram Bot, requirements and file setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from bot.main import main
+import logging
+
+# Load environment variables
+load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    main()
