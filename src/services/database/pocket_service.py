90a712416fa6cc250d3d5911df9e1ed0723232a5
TeoEchavarria <echavarria.0042@gmail.com>
2025-01-03 01:12:07 -0500

    Refactor create_pocket function to serialize model before insertion and improve get_pockets_by_user logic

--- src/services/database/pocket_service.py
+++ src/services/database/pocket_service.py
@@ -1,15 +1,15 @@
 from supabase import Client
 from models.pocket import Pocket
 from typing import Optional
+from utils.serialization import serialize_model
 
 def create_pocket(client: Client, pocket: Pocket) -> Optional[Pocket]:
-    response = client.table("pockets").insert(pocket.dict()).execute()
-    if response.status_code == 201:
-        return Pocket(**response.data[0])
-    return None
+    pocket = serialize_model(pocket)
+    response = client.table("pockets").insert(pocket).execute()
+    return Pocket(**response.data[0])
 
 def get_pockets_by_user(client: Client, user_id: str):
     response = client.table("pockets").select("*").eq("user_id", user_id).execute()
-    if response.status_code == 200:
+    if response.data != []:
         return [Pocket(**item) for item in response.data]
-    return []
+    return [create_pocket(client, Pocket(user_id=user_id, name="Default"))]
