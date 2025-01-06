0569279122313c4fd19759933698be9700216780
TeoEchavarria <echavarria.0042@gmail.com>
2025-01-05 21:31:30 -0500

    Refactor database service functions to use a centralized Supabase client connection

--- src/services/database/pocket_service.py
+++ src/services/database/pocket_service.py
@@ -2,19 +2,23 @@ from supabase import Client
 from models.pocket import Pocket
 from typing import Optional
 from utils.serialization import serialize_model
+from services.database_connection import get_supabase_client
 
-def create_pocket(client: Client, pocket: Pocket) -> Optional[Pocket]:
+def create_pocket_service(pocket: Pocket) -> Optional[Pocket]:
+    client = get_supabase_client()
     pocket = serialize_model(pocket)
     response = client.table("pockets").insert(pocket).execute()
     return Pocket(**response.data[0])
 
-def get_pockets_by_user(client: Client, user_id: str):
+def get_pockets_by_user(user_id: str):
+    client = get_supabase_client()
     response = client.table("pockets").select("*").eq("user_id", user_id).execute()
     if response.data != []:
         return [Pocket(**item) for item in response.data]
-    return [create_pocket(client, Pocket(user_id=user_id, name="Default"))]
+    return [create_pocket_service(Pocket(user_id=user_id, name="Default"))]
 
-def get_pocket_by_user_and_name(client: Client, user_id: str, pocket_name: str):
+def get_pocket_by_user_and_name(user_id: str, pocket_name: str):
+    client = get_supabase_client()
     response = client.table("pockets").select("*").eq("user_id", user_id).eq("name", pocket_name).execute()
     if response.data != []:
         return Pocket(**response.data[0])
--- src/services/database/user_service.py
+++ src/services/database/user_service.py
@@ -1,9 +1,11 @@
 from typing import Optional
+from services.database_connection import get_supabase_client
 
-def get_user_by_telegram_username(client, telegram_username: str) -> Optional[dict]:
+def get_user_by_telegram_username(telegram_username: str) -> Optional[dict]:
     """
     Aquí interactúas directamente con la base de datos (Supabase, Mongo, etc.)
     Devuelve un diccionario con datos del usuario o None si no existe.
     """
+    client = get_supabase_client()
     response = client.table("users").select("*").eq("username", telegram_username).single().execute()
     return response.data
